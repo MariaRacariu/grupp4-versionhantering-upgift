71b8b913b44f7759561005dabd97d8e1b99c8fa8
Abdulla Jamal <93247639+SweepCodes@users.noreply.github.com>
2024-01-09 16:39:11 +0100

    Implemented search function for home page.

--- script/main.js
+++ script/main.js
@@ -1,6 +1,21 @@
 // Import files and run functions
 import { toggleTheme } from "./modules/theme-toggle.js";
+import { weatherFetch } from "./modules/city-weather.js";
+import { displayData } from "./modules/city-weather.js";
+import { removePrevSearchResult } from "./modules/city-weather.js";
+import { errorHandler } from "./modules/city-weather.js";
 
+const form = document.querySelector('#searchCity_form')
+
+form.addEventListener('submit', searchHandler)
+
+async function searchHandler(event) {
+  event.preventDefault();
+  removePrevSearchResult
+  const searchInput = document.querySelector('#searchCity_input').value;
+  await weatherFetch(searchInput).then(displayData).catch(error);
+
+}
 // Add eventlistener to button in footer to run function
 const toggleThemeBtn = document
   .querySelector("#toggle-theme-button")
--- script/modules/city-weather.js
+++ script/modules/city-weather.js
@@ -1,1 +1,27 @@
-// Javascript for displaying the map from the weather api
+// Javascript for searching the weather by city name and displaying info.
+export async function weatherFetch(country) {
+    const apikey = '0c3cc91b87e044a264bca95e28a9e445'
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${apikey}&units=metric`
+    const response = await fetch(url)
+    console.log(response);
+    const data = await response.json()
+    console.log(data);
+    return data;
+}
+
+export function displayData(fetchdata) {
+    const cityEl = document.querySelector('#dataCity')
+    let tempratureEl = document.querySelector('#dataTemparature')
+    cityEl.innerText = fetchdata.name
+    tempratureEl.innerText = fetchdata.main.temp
+
+}
+
+export function removePrevSearchResult() {
+    const mainResultContainer = document.querySelector('.contentHeader');
+    mainResultContainer.innerHTML = '';
+}
+
+export function errorHandler(error) {
+
+}
