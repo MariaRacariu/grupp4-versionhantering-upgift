0068410c44c35d3f1cf08e5b23a52e07f87e36e0
lunayr <144939306+lunayr@users.noreply.github.com>
2024-01-10 12:39:13 +0100

    fixedNavigation

--- script/main.js
+++ script/main.js
@@ -23,23 +23,4 @@ const toggleThemeBtn = document
   .querySelector("#toggle-theme-button")
   .addEventListener("click", toggleTheme);
 
-/////////////////////////////////
-// NAVIGATION
-const navMenuList = document.querySelector(".nav-menu-list");
-const navigationBar = document.querySelector("#navigationBar");
-const hideMenuIcon = document.querySelector(".menu-hide-icon");
-const navMenuHide = document.querySelector(".nav-menu-list-hide");
-const logo = document.querySelector("#logo");
 
-navigationBar.addEventListener("click", function () {
-  navMenuList.classList.toggle("nav-menu-list-hide");
-  navMenuList.classList.add("display-flex");
-  logo.classList.add("hide");
-  navigationBar.classList.add("hide");
-});
-
-hideMenuIcon.addEventListener("click", function () {
-  navMenuList.classList.toggle("nav-menu-list-hide");
-  logo.classList.remove("hide");
-  navigationBar.classList.remove("hide");
-});
