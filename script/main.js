3866cf3a0bbb26013c70cfd3ad02844e1da98972
lunayr <144939306+lunayr@users.noreply.github.com>
2024-01-10 11:24:51 +0100

    smallfix

--- script/main.js
+++ script/main.js
@@ -8,14 +8,15 @@ import { errorHandler } from "./modules/city-weather.js";
 // cannot use this name, this name is a global name, it will affect the tags form in HTML in every file on the page
 // const form = document.querySelector('#searchCity_form')
 // form.addEventListener('submit', searchHandler);
-const searchForm = document.quearySelector('#searchCity_form').addEventListener('submit', searchHandler);
+const searchForm = document
+  .querySelector("#searchCity_form")
+  .addEventListener("submit", searchHandler);
 
 async function searchHandler(event) {
   event.preventDefault();
-  removePrevSearchResult
-  const searchInput = document.querySelector('#searchCity_input').value;
+  removePrevSearchResult;
+  const searchInput = document.querySelector("#searchCity_input").value;
   await weatherFetch(searchInput).then(displayData).catch(error);
-
 }
 // Add eventlistener to button in footer to run function
 const toggleThemeBtn = document
