4a85eb55ccb96990d7d28f42f94f1a914b36b67a
Abdulla Jamal <93247639+SweepCodes@users.noreply.github.com>
2024-01-11 13:56:11 +0100

    Icons for home page weather.

--- script/modules/city-weather.js
+++ script/modules/city-weather.js
@@ -9,6 +9,7 @@ export async function weatherFetch(country) {
     if (response.ok) {
         errorMsgEl.innerHTML = "";
         const data = await response.json();
+        console.log(data);
         return data;
     }
     else if (response.status === 404) throw 'not found'
@@ -18,8 +19,14 @@ export async function weatherFetch(country) {
 export function displayData(fetchdata) {
     const cityEl = document.querySelector('#dataCity');
     let tempratureEl = document.querySelector('#dataTemparature');
+    const iconEl = document.querySelector('#weatherIcon');
     cityEl.innerText = fetchdata.name;
     tempratureEl.innerHTML = `${fetchdata.main.temp.toFixed()} &#8451`;
+    for (const value of fetchdata.weather) {
+        const icon = value.icon;
+        iconEl.src = `https://openweathermap.org/img/wn/${icon}.png`
+    }
+
 
 }
 export function errorHandler(error) {
